f385ed6aadd606d0afcc748cbb4e1edde0e3532e
JosephRivera10 <joseph.rivera.09@gmail.com>
2018-02-05 10:52:22 -0600

    skeleton

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,35 @@
   
 
-  $('.chips').on('chip.add', function(e, chip){
-    // you have the added chip here
-  });
+//   $('.chips').on('chip.add', function(e, chip){
+//     // you have the added chip here
+//   });
 
-  $('.chips').on('chip.delete', function(e, chip){
-    // you have the deleted chip here
-  });
+//   $('.chips').on('chip.delete', function(e, chip){
+//     // you have the deleted chip here
+//   });
 
-  $('.chips').on('chip.select', function(e, chip){
-    // you have the selected chip here
-  });
-//access chip data
+//   $('.chips').on('chip.select', function(e, chip){
+//     // you have the selected chip here
+//   });
+// //access chip data
   
-var ingredient = $('.chip').val();
-console.log(ingredient);
-  $('.chip').material_chip('data-name');
-  console.log($(".chip").val());
+// var ingredient = $('.chip').val();
+// console.log(ingredient);
+//   $('.chip').material_chip('data-name');
+//   console.log($(".chip").val());
 
-    $('.chips-placeholder').material_chip({
-    placeholder: 'Enter a tag',
-    secondaryPlaceholder: '+Tag',
-  });
-  $('.chips-autocomplete').material_chip({
-    autocompleteOptions: {
-      data: {
-        'Apple': null,
-        'Microsoft': null,
-        'Google': null
-      },
-      limit: Infinity,
-      minLength: 1
-    }
-  });
+//     $('.chips-placeholder').material_chip({
+//     placeholder: 'Enter a tag',
+//     secondaryPlaceholder: '+Tag',
+//   });
+//   $('.chips-autocomplete').material_chip({
+//     autocompleteOptions: {
+//       data: {
+//         'Apple': null,
+//         'Microsoft': null,
+//         'Google': null
+//       },
+//       limit: Infinity,
+//       minLength: 1
+//     }
+//   });
