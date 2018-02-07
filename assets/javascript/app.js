3901d0d5389717681484026efc7ec6530f86cd67
JosephRivera10 <joseph.rivera.09@gmail.com>
2018-02-07 10:09:03 -0600

    new features

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
   
+var queryURL;
+
 //adds ingredients to ingredient field as a new chip
   $(document).on("click", '#add', function(event) {
     event.preventDefault();
@@ -26,28 +28,65 @@ var ingredientsSearchArray = [];
 $(".chip").each(function () {
   ingredientsSearchArray.push($(this).data("name"));
 });
-// var ingredientsSearch = $(".chip").data("name");
-console.log(ingredientsSearchArray);
-
- var queryURL = "https://api.edamam.com/search?q=" + ingredientsSearchArray + "&app_id=5148c2dc&app_key=1a2daaf08f5a479ca7f99584442c2dbd&from=0&to=10";
- console.log(queryURL);
-
+    
+    var lowFat = $("#test3:checked").val();
+    var vegetarian = $("#test2:checked").val();
+    var peanut = $("#test1:checked").val();
+    if(peanut == undefined && vegetarian == undefined && lowFat == undefined) {
+      queryURL = "https://api.edamam.com/search?q=" + ingredientsSearchArray + "&app_id=5148c2dc&app_key=1a2daaf08f5a479ca7f99584442c2dbd&from=0&to=10";
+      runsearch(queryURL);   
+    }
+    else if (vegetarian == "vegetarian" && peanut == "peanut-free" && lowFat == "low-fat") {
+      queryURL = "https://api.edamam.com/search?q=" + ingredientsSearchArray + "&app_id=5148c2dc&app_key=1a2daaf08f5a479ca7f99584442c2dbd&from=0&to=10&health=" + peanut + "&health=" + vegetarian + "&diet=" + lowFat + "";
+      runsearch(queryURL);
+    }
+    else if (vegetarian == "vegetarian" && peanut == "peanut-free") {
+      queryURL = "https://api.edamam.com/search?q=" + ingredientsSearchArray + "&app_id=5148c2dc&app_key=1a2daaf08f5a479ca7f99584442c2dbd&from=0&to=10&health=" + vegetarian + "&health=" + peanut + "";
+      runsearch(queryURL);
+    }
+    else if (vegetarian == "vegetarian" && lowFat == "low-fat") {
+      queryURL = "https://api.edamam.com/search?q=" + ingredientsSearchArray + "&app_id=5148c2dc&app_key=1a2daaf08f5a479ca7f99584442c2dbd&from=0&to=10&health=" + vegetarian + "&diet=" + lowFat + "";
+      runsearch(queryURL);
+    }
+    else if (peanut == "peanut-free" && lowFat == "low-fat") {
+      queryURL = "https://api.edamam.com/search?q=" + ingredientsSearchArray + "&app_id=5148c2dc&app_key=1a2daaf08f5a479ca7f99584442c2dbd&from=0&to=10&health=" + peanut + "&diet=" + lowFat + "";
+      runsearch(queryURL);
+    }
+    else if (peanut == "peanut-free") {
+      //run peanut query
+       queryURL = "https://api.edamam.com/search?q=" + ingredientsSearchArray + "&app_id=5148c2dc&app_key=1a2daaf08f5a479ca7f99584442c2dbd&from=0&to=10&health=" + peanut + "";
+      runsearch(queryURL);
+    }
+    else if (vegetarian == "vegetarian") {
+      queryURL = "https://api.edamam.com/search?q=" + ingredientsSearchArray + "&app_id=5148c2dc&app_key=1a2daaf08f5a479ca7f99584442c2dbd&from=0&to=10&health=" + vegetarian + "";
+      runsearch(queryURL);
+    }
+    else if (lowFat == "low-fat") {
+      queryURL = "https://api.edamam.com/search?q=" + ingredientsSearchArray + "&app_id=5148c2dc&app_key=1a2daaf08f5a479ca7f99584442c2dbd&from=0&to=10&diet=" + lowFat + "";
+      runsearch(queryURL);
+    }
+      
+  });
+    
+    function runsearch(){
     $.ajax({
       url: queryURL,
       method: 'GET'
     })
     .then(function(response) {
+      console.log(queryURL)
     
     var results = response.hits;
-    console.log(results);
 
     for (var i = 0; i < response.hits.length; i++) {
 
       // var image = "";
       // var recipeTitle = ""; 
       // var link = "";
-      var newCard = $("<div>");
+      var newCard = $("<div>").attr("id", "card");
       newCard.addClass("card");
+      newCard.addClass("small");
+      newCard.addClass("col s12 m5");
 
       var cardDesign = $("<div>");
       cardDesign.addClass("card-image");
@@ -55,52 +94,63 @@ console.log(ingredientsSearchArray);
       cardDesign.addClass("waves-block");
       cardDesign.addClass("waves-light");
 
-      var image = $("<img>");
+      var image = $("<img>").attr("id", "image");
       image.attr("src", results[i].recipe.image);
       image.addClass("activator");
-      image.width(300);
-      image.height(300);
-      console.log(image);
+      image.width(280);
+      image.height(180);
 
       var newCardContent = $("<div>");
       newCardContent.addClass("card-content");
 
-      var recipeTitle = $("<span>");
+      var recipeTitle = $("<span>").attr("id", "span");
       recipeTitle.text(results[i].recipe.label);
       recipeTitle.addClass("card-title");
       recipeTitle.addClass("activator");
-      recipeTitle.addClass("grey-text");
-      recipeTitle.addClass("text-darken-4");
-      console.log(recipeTitle);
+      recipeTitle.addClass("teal-text");
+      recipeTitle.addClass("text-darken-3");
 
-      var showIngredients = $("<i>");
+      var showIngredients = $("<i>").attr("id", "i");
       showIngredients.addClass("material-icons");
       showIngredients.addClass("right");
-      showIngredients.text("Ingredients");
+      showIngredients.addClass("teal-text text-darken-3")
+      showIngredients.text("...")
 
-      var link = $("<a>");
+      var space = $("<p>");
+
+      var link = $("<a>").attr("id", "a");
       link.attr("href", results[i].recipe.url);
+      link.addClass("orange-text text-darken-3");
       link.text("Recipe Link");
       // link.target("_blank");
-      console.log(link);
 
-      // for (var j = 0; j < results[i].recipe.ingredientLines.length; j++) {
-      //         $("#recipeIngredientsDiv"+k).append('<li>' + results[i].recipe.ingredientLines[j] + '</li>');
-
+      
       var revealCard = $("<div>");
       revealCard.addClass("card-reveal");
+      revealCard.addClass("teal lighten-2");
 
-      var hideIngredients = $("<i>");
+
+      var hideIngredients = $("<i>").attr("id", "i2");
       hideIngredients.addClass("material-icons");
       hideIngredients.addClass("right");
-      hideIngredients.text("Hide Ingredients");
+      hideIngredients.addClass("orange-text text-darken-4")
+      hideIngredients.text("...");
 
-      var ingredientList = $("<span>");
-      ingredientList.text(results[i].recipe.ingredientLines);
+      for (var j = 0; j < results[i].recipe.ingredientLines.length; j++) {
+      //         $("#recipeIngredientsDiv"+k).append('<li>' + results[i].recipe.ingredientLines[j] + '</li>');
+      var newLine = $("<p>");
+
+      var ingredientList = $("<span>").attr("id", "span");
+      ingredientList.text(results[i].recipe.ingredientLines[j]);
       ingredientList.addClass("card-title");
       ingredientList.addClass("activator");
-      ingredientList.addClass("grey-text");
-      ingredientList.addClass("text-darken-4");
+      ingredientList.addClass("teal-text");
+      ingredientList.addClass("text-darken-3");
+    
+    $(revealCard).append(newLine);
+    newLine.append(ingredientList);
+    }
+
 
 
       $("#cardArea").append(newCard);
@@ -108,13 +158,15 @@ console.log(ingredientsSearchArray);
       cardDesign.append(image);
       newCard.append(newCardContent);
       newCardContent.append(recipeTitle);
-      recipeTitle.append(link);
-      recipeTitle.append(showIngredients);
+      recipeTitle.append(space);
+      space.append(link);
+      space.append(showIngredients);
       newCard.append(revealCard);
-      revealCard.append(ingredientList);
+      // revealCard.append(newLine);
+      // newLine.append(ingredientList);
       ingredientList.append(hideIngredients);
     }
 
     });
 
-  });
+  };
